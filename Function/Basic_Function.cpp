65024cdd2142ca0d4159b2aa3d2c87883610cfa8
Chotipat Poowongthanarat <top_classic@hotmail.com>
2016-10-28 12:45:48 -0700

    Update Call_Value_Function.cpp
    return value (char)

--- Function/Basic_Function.cpp
+++ Function/Basic_Function.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-long calculate(unsigned int num1, unsigned int num2); //Function declaration
+long calculate(unsigned int num1, unsigned int num2); // Function declaration
 
 int main() {
 
-cout << "Calculate: " << calculate(400, 200) << endl; //Call function and print output
+cout << "Calculate: " << calculate(400, 200) << endl; // Call function and print output
 system("PAUSE");
 return 0;
 }
 
-long calculate(unsigned int num1, unsigned int num2) { //return_type function_name( parameter list ) {body of the function }
+long calculate(unsigned int num1, unsigned int num2) { // return_type function_name( parameter list ) {body of the function }
 
 int resualt;
 
--- Function/Call_Value_Function.cpp
+++ Function/Call_Value_Function.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
-void swap(char one, char two);
+void swap(char *one, char *two, char *three); // char *one = pointer that points to the "one" string. char one[] = creating an array of 3 bytes to store {'o','n','e'}
 
-void swap(char one, char two) {
+void swap(char *one, char *two, char *three) {
 
+	char threex[sizeof(one)];
 
+	strcpy_s(threex, sizeof(one), one); // Save data frome one to threex
+	strncpy(one, three, 6); // Change data frome three to one (6 is size of array)
+	strcpy_s(three, sizeof(threex), threex); // Change data frome treex to three (It's one)
 
+	return; // Return value char one, two, three
 }
 
 int main() {
 
+	char one[6] = "one"; // one[6] equals three[6] for coppy data
+	char two[4] = "two"; // 4 - 1 = 3 character ( 1 is \0)
+	char three[6] = { 't','h','r','e','e','\0' }; // \0 end text
 
+	cout << "After Swap: " << one << " " << two << " " << three << endl;
+
+	swap(one, two ,three);
+
+	cout << "Before Swap: " << one << " " << two << " " << three << endl;
 
 	system("PAUSE");
+
 	return 0;
 }
